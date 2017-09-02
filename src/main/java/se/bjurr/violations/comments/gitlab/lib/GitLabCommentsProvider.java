52202ad77747e8f81d3a1f9a34bc9f03d7cc748c
Tomas Bjerre <tomas.bjerre85@gmail.com>
2017-09-02 15:49:47 +0200

    [Gradle Release Plugin] - pre tag commit:  '1.13'.

--- src/main/java/se/bjurr/violations/comments/gitlab/lib/GitLabCommentsProvider.java
+++ src/main/java/se/bjurr/violations/comments/gitlab/lib/GitLabCommentsProvider.java
@@ -7,7 +7,6 @@ import static se.bjurr.violations.comments.lib.utils.CommentsUtils.escapeHTML;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.gitlab.api.AuthMethod;
 import org.gitlab.api.GitlabAPI;
 import org.gitlab.api.TokenType;
@@ -17,7 +16,6 @@ import org.gitlab.api.models.GitlabNote;
 import org.gitlab.api.models.GitlabProject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import se.bjurr.violations.comments.lib.model.ChangedFile;
 import se.bjurr.violations.comments.lib.model.Comment;
 import se.bjurr.violations.comments.lib.model.CommentsProvider;
