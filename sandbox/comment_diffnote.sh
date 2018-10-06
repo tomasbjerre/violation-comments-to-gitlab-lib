79819367fc02947e770f9afd102dbe964c6470bf
Patrizio Bonzani <bonzani@users.noreply.github.com>
2018-10-06 14:33:08 +0200

    make parameters query parameters.

--- sandbox/comment_diffnote.sh
+++ sandbox/comment_diffnote.sh
@@ -2,4 +2,4 @@ project=tomas.bjerre85%2Fviolations-test
 mergeRequestIid=1
 url=https://gitlab.com/api/v4/projects/$project/merge_requests/$mergeRequestIid/discussions
 
-curl $url -H 'Content-Type", "application/json' -H "PRIVATE-TOKEN: $GITLAB_TOKEN" --data '@comment_diffnote.txt' -v 2>&1
+curl $url --request POST --header "PRIVATE-TOKEN: $GITLAB_TOKEN" --data "$(cat comment_diffnote.txt | tr '\n' '&')" -v 2>&1
